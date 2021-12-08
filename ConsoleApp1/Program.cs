3f03ace15877db083c3fc96701214e44f8641760
Said G. Mikayilov <saidgm@code.local>
2021-12-08 10:43:22 +0400

    Extention

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Extention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Salam Cavid.Necesen Cavid?";
+            Console.WriteLine(str.CheckString("Cavid"));
+        }
+    }
+}
